a622e60ff73e8769b9e9d3ff62c0cbb0f5d911a5
Mateus Medeiros <mateus.medeiros@radixeng.com>
2026-07-22 15:05:32 -0300

    Add initial hub

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using SignalRChat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -22,5 +26,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
